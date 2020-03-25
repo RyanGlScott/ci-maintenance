9a74773ce381061b8866ed83f968625ca196b92c
Ryan Scott <ryan.gl.scott@gmail.com>
2020-03-25 11:06:28 -0400

    GHC 8.10

--- src/Main.hs
+++ src/Main.hs
@@ -538,6 +538,7 @@ supportedGhcVersions =
   , (("8","4"),  ([8,4,4],  Nothing))
   , (("8","6"),  ([8,6,5],  Nothing))
   , (("8","8"),  ([8,8,3],  Nothing))
+  , (("8","10"), ([8,10,1], Nothing))
   ]
 
 printBanner :: Char -> IO ()
